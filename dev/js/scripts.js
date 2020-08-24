86d22ca12f099a72a1ee7dc7bb8bbd91a6b92c96
Safe28 <63374759+Safe28@users.noreply.github.com>
2020-08-23 23:47:28 -0400

    Added intitial js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
+// @codekit-prepend "main-nav/nav-global.js";
+// @codekit-prepend "main-nav/burger-click.js";
+// @codekit-prepend "main-nav/main-nav-click.js";
 
+$( document ).ready(function() {
+
+    $("#show-form-btn").on("click", function(){
+       //console.log("button clicked");
+ 
+       // hide contact-button-section
+       $("#contact-button-section").hide();
+ 
+       // show form container
+       $("#form-container").show();
+ 
+    });
+ 
+ });
