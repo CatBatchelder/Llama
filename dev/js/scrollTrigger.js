7d6dce868451dcbfe9c8b4a24b8ede3f83e3fd5a
khuongbhan <hbk32@drexel.edu>
2020-08-30 01:37:56 -0400

    section 1 completed

--- dev/js/scrollTrigger.js
+++ dev/js/scrollTrigger.js
@@ -160,7 +160,7 @@ ScrollTrigger.create({
   // end: "bottom 90%",
   animation: careLlama1TimeLine,
   toggleActions: "play none none none",
-  markers: true,
+  // markers: true,
   // scrub: 1,
   id: "cream-llama"
 });
@@ -185,6 +185,138 @@ ScrollTrigger.create({
 });
 
 
+//para2
+var carePara2TimeLine = gsap.timeline();
+
+carePara2TimeLine.from("#para-2", {duration: bannerSpeed, alpha: 0, yPercent:-50}, "para2")
+
+ScrollTrigger.create({
+  trigger: "#para-2",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: carePara2TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-paragraph2"
+});
+
+//footsteps group2
+var careFootstepsGroup2TimeLine = gsap.timeline();
+
+careFootstepsGroup2TimeLine.from("#footstep-4", {duration: 0.75, alpha: 0}, "footstep4")
+
+ScrollTrigger.create({
+  trigger: "#footstep-4",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: careFootstepsGroup2TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-footsteps2"
+});
+
+//para3
+var carePara3TimeLine = gsap.timeline();
+
+carePara3TimeLine.from("#para-3", {duration: bannerSpeed, alpha: 0, yPercent:-50}, "para3")
+
+ScrollTrigger.create({
+  trigger: "#para-3",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: carePara3TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-paragraph3"
+});
+
+
+//footsteps group3
+var careFootstepsGroup3TimeLine = gsap.timeline();
+
+careFootstepsGroup3TimeLine.from("#footstep-5", {duration: 0.75, alpha: 0}, "footstep5")
+                           .from("#footstep-6", {duration: 0.75, alpha: 0, yPercent:-50}, "footstep6")
+
+ScrollTrigger.create({
+  trigger: "#footstep-5",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: careFootstepsGroup3TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-footsteps3"
+});
+
+//para4
+var carePara4TimeLine = gsap.timeline();
+
+carePara4TimeLine.from("#para-4", {duration: bannerSpeed, alpha: 0, xPercent:50}, "para4")
+
+ScrollTrigger.create({
+  trigger: "#para-4",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: carePara4TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-paragraph4"
+});
+
+
+//footsteps group4
+var careFootstepsGroup4TimeLine = gsap.timeline();
+
+careFootstepsGroup4TimeLine.from("#footstep-7", {duration: 0.75, alpha: 0}, "footstep7")
+
+ScrollTrigger.create({
+  trigger: "#footstep-7",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: careFootstepsGroup4TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "care-footsteps4"
+});
+
+
+//brown llama
+var careLlama2TimeLine = gsap.timeline();
+
+careLlama2TimeLine.from("#llama-brown", {duration: bannerSpeed, alpha: 0, xPercent: -70}, "llamaBrown")
+
+ScrollTrigger.create({
+  trigger: "#llama-brown",
+  start: "top 30%",
+  // end: "bottom 90%",
+  animation: careLlama2TimeLine,
+  toggleActions: "play none none none",
+  // markers: true,
+  // scrub: 1,
+  id: "brown-llama"
+});
+
+
+//para5
+var carePara5TimeLine = gsap.timeline();
+
+carePara5TimeLine.from("#para-5", {duration: bannerSpeed, alpha: 0, yPercent:50}, "para5")
+
+ScrollTrigger.create({
+  trigger: "#para-5",
+  start: "top 50%",
+  // end: "bottom 90%",
+  animation: carePara5TimeLine,
+  toggleActions: "play none none none",
+  markers: true,
+  // scrub: 1,
+  id: "care-paragraph5"
+});
 
 
 
