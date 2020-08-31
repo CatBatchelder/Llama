23e4ada4e336a80633f08a46b966cf576ccbff4f
Safe28 <63374759+Safe28@users.noreply.github.com>
2020-08-31 15:37:47 -0400

    Added js navigation

--- dev/js/main-nav/nav-scroll.js
+++
@@ -1,7 +0,0 @@
-var scrollItems =["#gallery", "#section-2"];
-
-console.log(scrollItems[2]);
-
-function scrollIndex(){
-    gsap.to(window, {duration: 2, scrollTo:{ y: scrollItems[$('#main-nav ul li').index(this)], offsetY: 60}});
-}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,25 +1,10 @@
 // @codekit-prepend "scrollTriggerReg.js";
 // @codekit-prepend "scrollTrigger.js";
 // @codekit-prepend "konva.js";
-// @codekit-prepend "main-nav/burger-animation.js";
-// @codekit-prepend "main-nav/burger-mouse-events.js";
 // @codekit-prepend "main-nav/nav-global.js";
 // @codekit-prepend "main-nav/burger-click.js";
 // @codekit-prepend "main-nav/main-nav-click.js";
-// @codekit-prepend "main-nav/nav-scroll.js";
-
 
 $( document ).ready(function() {
 
-    $("#show-form-btn").on("click", function(){
-       //console.log("button clicked");
- 
-       // hide contact-button-section
-       $("#contact-button-section").hide();
- 
-       // show form container
-       $("#form-container").show();
- 
-    });
- 
- });
+});
