21d5fd70dd18915d7a1f2d413c31587a73fa22e1
eugeniekwon <eugenie.kwon@gmail.com>
2020-08-31 12:04:11 +0900

    imported konva, edited other sections together as a group
    - konva
    - map section (title)
    - nav margins

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,13 @@
 // @codekit-prepend "scrollTriggerReg.js";
 // @codekit-prepend "scrollTrigger.js";
+// @codekit-prepend "konva.js";
 // @codekit-prepend "main-nav/burger-animation.js";
 // @codekit-prepend "main-nav/burger-mouse-events.js";
 // @codekit-prepend "main-nav/nav-global.js";
 // @codekit-prepend "main-nav/burger-click.js";
 // @codekit-prepend "main-nav/main-nav-click.js";
 // @codekit-prepend "main-nav/nav-scroll.js";
+
 
 $( document ).ready(function() {
 
